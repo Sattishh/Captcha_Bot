09e7da6d3c2cc0f622ace2216e9225c5cdae17a8
Sattish Shewkani <spshewkani@gmail.com>
2020-03-07 15:01:19 +0700

    Files uploaded

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+    console.log("Ready!");
+    client.user.setStatus('dnd');
+    client.user.setActivity(';help | ;invite', { type :3});
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Your bot is in ${client.guilds.size} servers!`)
+    
+    
+})
+
+client.on('message', message => {
+
+})
+client.login("Njg0MDEyNTk2MzY1NjIzMzUw.XmNQ3w.4KVj_-yONYOylcVnFFCXJtqRHdI")
