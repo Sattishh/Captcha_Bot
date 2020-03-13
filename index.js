318513b4800f33214d086cb3010cb92e313a8586
Sattish Shewkani <spshewkani@gmail.com>
2020-03-13 23:57:23 +0700

    major updates

--- index.js
+++ index.js
@@ -2,17 +2,45 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
+
+
 client.on("ready", () => {
-    console.log("Ready!");
-    client.user.setStatus('dnd');
-    client.user.setActivity(';help | ;invite', { type :3});
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`Your bot is in ${client.guilds.size} servers!`)
-    
-    
+    let guild = client.users.size
+    console.log(guild)
+let statuses = [`In Development :construction:`,';help | ;invite'];
+    setInterval(function(){
+        let status = statuses[Math.floor(Math.random()*statuses.length)];
+        // using setPresence()
+        client.user.setPresence({ game: { name:status},status: 'dnd' });
+        // client.user.setPresence({ activity: { name: status }, status:'online' });
+
+    }, 10000) // Runs interval every 10 seconds
 })
 
 client.on('message', message => {
+        if (message.content.startsWith(';dm')) {
+         message.author.send("You\'re a chicken buddy")
+         message.channel.send("Sent you something :smirk:")
+        }
+        if (message.content === '95inb') {
+            // Send "pong" to the same channel
+            message.channel.send('Congratulations, that\'s the correct captcha. You will soon get the server role!');
+            message.ch.send(';role')
+        } 
+        const ch = member.guild.channels.cache.find(ch => ch.name === 'join-channel');
+        if (!ch) return;
 
-})
+
+    
+});
+
+client.on('guildMemberAdd', member => {
+    member.send("Please fill this captcha to access the server");
+    member.send("https://i.imgur.com/vyAxtWU.png")
+  
+     var role = message.guild.roles.find(role => role.id === "598880700074033212");
+    
+    message.member.addRole(role);
+
+});
 client.login(process.env.token)
